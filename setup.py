5af04786a9e764398d47987f8c7d4227ccb22938
Philip Olenyk <philya@gmail.com>
2019-07-01 19:34:46 +0300

    Minimal package scaffolding

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ljbq',
+      version='0.1',
+      description='Parametrized Locally Cached BigQuery Queries',
+      url='http://github.com/philya/ljbq',
+      author='Philip Olenyk',
+      author_email='philya@gmail.com',
+      license='MIT',
+      packages=['ljbq'],
+      zip_safe=False)
