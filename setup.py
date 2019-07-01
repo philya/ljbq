2339a3e0ce9d918e68d7e38e693a0c9502cd3668
Philip Olenyk <philya@gmail.com>
2019-07-02 02:28:14 +0300

    Fix a typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='ljbq',
       version='0.1',
-      description='Parametrized Locally Cached BigQuery Queries',
+      description='Parameterized Locally Cached BigQuery Queries',
       url='http://github.com/philya/ljbq',
       author='Philip Olenyk',
       author_email='philya@gmail.com',
